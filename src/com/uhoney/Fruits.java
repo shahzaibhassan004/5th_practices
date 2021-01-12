9af3316c4f3852277de9abccfadbe244435dd8a1
HONEY <shahzaibhassan004@gmail.com>
2021-01-12 10:56:42 +0500

    hello

---
+++ src/com/uhoney/Fruits.java
@@ -0,0 +1,18 @@
+package com.uhoney;
+
+public class Fruits {
+
+    String fruit;
+
+    public Fruits( String name) {
+        fruit = name;
+
+    }
+
+    public String getFruit()
+    {
+
+        return fruit;
+    }
+
+}
---
+++ src/com/uhoney/Main.java
@@ -0,0 +1,12 @@
+package com.uhoney;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// classes practice within java
+        Fruits fruits;
+        fruits = new Fruits("ali");
+        String f =fruits.getFruit();
+        System.out.println(f);
+    }
+}
